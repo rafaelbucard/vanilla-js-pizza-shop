69b0db21df3a9498e0ed04d474d670427ec955e3
Rafael Pereira Buçard <rafaelbucard@gmail.com>
2020-09-21 20:40:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,23 @@ pizzaJson.map((item, index)=>{
 
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
     // Seleção do html display none para uma areá de pizzas 
-    pizzaItem.setAtribute('data-key',index);
+
+    pizzaItem.setAttribute('data-key', index); // Criando atributo para identificar pizza
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('a').addEventListener('click',(e)=>{
         e.preventDefault();
+        //Pegando key da pizza
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        doc('.pizzaBig img').src = pizzaJson[key].img;
+        doc('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        doc('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        doc('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        docAll('.pizzaInfo--size').forEach((size,sizeIndex)=>{
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
         doc('.pizzaWindowArea').style.opacity = 0;
         doc('.pizzaWindowArea').style.display = 'flex';
         setTimeout(()=>{
